0a990a2f2e0b389218a43071dd5a2fbaa27b3827
secretppl608 <lacak3736@gmail.com>
2025-09-26 22:18:11 +0800

    bb

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,24 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins:
+    process.env.NODE_ENV === 'production'
+      ? [
+        'postcss-flexbugs-fixes',
+        [
+          'postcss-preset-env',
+          {
+            autoprefixer: {
+              flexbox: 'no-2009',
+            },
+            stage: 3,
+            features: {
+              'custom-properties': false,
+            },
+          },
+        ],
+      ]
+      : [
+        // 开发环境不应用转换
+      ],
 };
 
 export default config;
