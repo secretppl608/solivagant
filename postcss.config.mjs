19ed0b8ad68f0b951f1e765b56c1a007057f5e6a
secretppl608 <lacak3736@gmail.com>
2025-09-27 21:41:02 +0800

    更新样式文件，添加全局颜色变量和响应式设计支持

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,24 +1,24 @@
 const config = {
   plugins:
-    process.env.NODE_ENV === 'production'
+    process.env.NODE_ENV === "production"
       ? [
-        'postcss-flexbugs-fixes',
-        [
-          'postcss-preset-env',
-          {
-            autoprefixer: {
-              flexbox: 'no-2009',
+          "postcss-flexbugs-fixes",
+          [
+            "postcss-preset-env",
+            {
+              autoprefixer: {
+                flexbox: "no-2009",
+              },
+              stage: 3,
+              features: {
+                "custom-properties": false,
+              },
             },
-            stage: 3,
-            features: {
-              'custom-properties': false,
-            },
-          },
-        ],
-      ]
+          ],
+        ]
       : [
-        // 开发环境不应用转换
-      ],
+          // 开发环境不应用转换
+        ],
 };
 
 export default config;
